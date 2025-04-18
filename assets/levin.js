749030bc547d6f54ea8f0d09cac5141bcc775948
Lucifer <magicdevstar@gmail.com>
2025-04-18 16:37:59 +1000

    announce-bar, header-announce-bar complete

--- assets/levin.js
+++ assets/levin.js
@@ -5,12 +5,31 @@ $(document).ready(function () {
     infinite: true,
     arrows: true,
   });
-  for(let i = 3; i < 8; i ++){
+  for (let i = 3; i < 8; i++) {
     $(`.collection-list-slider--${i}`).slick({
       slidesToShow: i,
       slidesToScroll: 1,
       infinite: false,
       arrows: true,
+      prevArrow:
+        '<button type="button" class="collection--slick-arrow collection--slick-prev-arrow">&#10094;</button>',
+      nextArrow:
+        '<button type="button" class="collection--slick-arrow collection--slick-next-arrow">&#10095;</button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: "unslick",
+        },
+      ],
     });
   }
-})
+});
